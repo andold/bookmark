5029ccbdaeb7671b20a0232056abd0473c9127ee
andold <andold@naver.com>
2024-11-03 19:48:08 +0900

    backup #2

--- src/main/java/kr/andold/bookmark/ScheduledTasks.java
+++ src/main/java/kr/andold/bookmark/ScheduledTasks.java
@@ -1,14 +1,24 @@
 package kr.andold.bookmark;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import kr.andold.bookmark.domain.BookmarkParam;
 import kr.andold.bookmark.service.BookmarkService;
 import kr.andold.utils.Utility;
-import lombok.Setter;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -17,11 +27,46 @@ import lombok.extern.slf4j.Slf4j;
 @ConditionalOnProperty(value = "app.scheduling.enable", havingValue = "true", matchIfMissing = true)
 public class ScheduledTasks {
 	@Autowired
-	private BookmarkService bookmarkService;
+	private BookmarkService service;
+
+	@Getter private static String dataPath;
+	@Value("${data.path:C:/tmp}")
+	public void setDataPath(String dataPath) {
+		log.info("{} setDataPath({})", Utility.indentMiddle(), dataPath);
+		ScheduledTasks.dataPath = dataPath;
+		File directory = new File(dataPath);
+		if (!directory.exists()) {
+			log.info("{} NOT EXIST PATH setDataPath({})", Utility.indentMiddle(), dataPath);
+			directory.mkdir();
+		}
+	}
 
 	// 매일
-	@Scheduled(fixedDelay = 1000 * 60 * 60 * 24, initialDelay = 1000 * 60 * 60)
-	public void scheduleTaskEveryDays() {
+	@Scheduled(cron = "0 47 0 * * *")
+	public void scheduleTaskDaily() {
+		log.info("{} scheduleTaskDaily()", Utility.indentStart());
+		long started = System.currentTimeMillis();
+		
+		String yyyymmdd = LocalDate.now().minusDays(1).format(DateTimeFormatter.BASIC_ISO_DATE);
+
+		BookmarkParam param = service.download();
+		String text = Utility.toStringJsonPretty(param);
+		String filenameCurrent = String.format("%s/bookmark.json", dataPath);
+		String filenameYesterday = String.format("%s/bookmark-%s.json", dataPath, yyyymmdd);
+		rename(filenameCurrent, filenameYesterday);
+		Utility.write(filenameCurrent, text);
+
+		log.info("{} scheduleTaskDaily() - {}", Utility.indentEnd(), Utility.toStringPastTimeReadable(started));
+	}
+
+	private void rename(String before, String after) {
+		try {
+			Path oldfile = Paths.get(before);
+			Path newfile = Paths.get(after);
+			Files.move(oldfile, newfile);
+		} catch (IOException e) {
+			log.error("{} IOException rename(『{}』, 『{}』) - 『{}』", Utility.indentMiddle(), before, after, e.getLocalizedMessage(), e);
+		}
 	}
 
 }
---
+++ src/test/java/kr/andold/bookmark/ScheduledTasksTest.java
@@ -0,0 +1,21 @@
+package kr.andold.bookmark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import kr.andold.utils.Utility;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ScheduledTasksTest {
+	@Autowired private ScheduledTasks service;
+
+	@Test
+	public void testScheduleTaskDaily() {
+		log.info("{} testScheduleTaskDaily() -----------------------------------------------------------", Utility.indentMiddle());
+		service.scheduleTaskDaily();
+	}
+
+}
